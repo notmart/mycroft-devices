2bf38ab75d8189ee0aed7a9a5a6dd1720d61082d
Åke Forslund <ake.forslund@gmail.com>
2019-04-25 11:38:06 +0200

    create mycroft log folder

--- scripts/92-clone-mycroft.sh
+++ scripts/92-clone-mycroft.sh
@@ -3,5 +3,7 @@ USER=respeaker
 
 mv /mycroft-core /home/respeaker/mycroft-core
 mkdir -p /opt/mycroft/skills
+mkdir -p /var/log/mycroft
 chown -R $UGID:$UGID /home/$USER
 chown -R $UGID:$UGID /opt/mycroft
+chown -R $UGID:$UGID /var/log/mycroft
