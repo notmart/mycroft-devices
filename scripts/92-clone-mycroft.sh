914bd166f4f50c8408f03ede4db9062a6454184d
Åke Forslund <ake.forslund@gmail.com>
2019-04-25 11:53:29 +0200

    Add config to auto-update mycroft if needed

--- scripts/92-clone-mycroft.sh
+++ scripts/92-clone-mycroft.sh
@@ -2,6 +2,9 @@ UGID=32011
 USER=respeaker
 
 mv /mycroft-core /home/respeaker/mycroft-core
+# Set to auto-update to run dev_setup.sh by default
+mv /.dev_opts.json /home/respeaker/mycroft-core
+
 mkdir -p /opt/mycroft/skills
 mkdir -p /var/log/mycroft
 chown -R $UGID:$UGID /home/$USER
